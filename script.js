e4e541223afb9b94ac74f247a52262d1cb3524d2
Adriel Santos <109179771+adriel-mp3@users.noreply.github.com>
2022-12-03 14:44:43 -0300

    add files

---
+++ script.js
@@ -0,0 +1,28 @@
+const filter = document.querySelector("input");
+const cards = document.querySelectorAll(".cards li");
+document.addEventListener("input", filterCards);
+
+function filterCards() {
+  if (filter.value !== "") {
+    cards.forEach((card) => {
+      let cardHeading = card.querySelector("h2");
+      
+      cardHeading = cardHeading.textContent.toLowerCase();
+      let filterText = filter.value.toLowerCase();
+      
+      if (!cardHeading.includes(filterText)) {
+        // hide the card element
+        card.style.display = "none";
+      }
+      else {
+        card.style.display = "block";
+      }
+    });
+    
+  }
+  else {
+    cards.forEach((card) => {
+      card.style.display = "block"
+    }) 
+  }
+}
